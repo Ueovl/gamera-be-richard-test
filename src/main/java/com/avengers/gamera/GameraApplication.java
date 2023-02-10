3cf7649a7fb27a9d8c053c32990e03ef4a4c09d7
lawrenceliao <lawrenceltq@gmail.com>
2023-02-10 19:59:09 +1100

    modified-existing-config-and-added-new-ones

--- src/main/java/com/avengers/gamera/GameraApplication.java
+++ src/main/java/com/avengers/gamera/GameraApplication.java
@@ -3,11 +3,18 @@ package com.avengers.gamera;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class GameraApplication {
-
+	private static final String TIME_ZONE = "UTC";
 	public static void main(String[] args) {
 		SpringApplication.run(GameraApplication.class, args);
 	}
 
+	@PostConstruct
+	public void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone(TIME_ZONE));
+	}
 }
