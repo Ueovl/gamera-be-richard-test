d6f5dbc697dfd0445d246938819687636d0523e2
Kelozheng <100518138+Kelozheng@users.noreply.github.com>
2023-02-20 00:28:16 +1100

    Cors: update config

--- src/main/java/com/avengers/gamera/config/SecurityConfig.java
+++ src/main/java/com/avengers/gamera/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -35,6 +36,9 @@ public class SecurityConfig {
                     cors.setAllowedHeaders(allowedHeaders);
                     return cors;
                 })
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authorizeRequests()
                 .antMatchers("/**").permitAll()
