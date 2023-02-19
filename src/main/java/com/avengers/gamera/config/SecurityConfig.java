8c80368cc56d6f2bf0fe8214dc739296f46d5cd8
Kelozheng <100518138+Kelozheng@users.noreply.github.com>
2023-02-20 00:17:29 +1100

    Cors: config

---
+++ src/main/java/com/avengers/gamera/config/SecurityConfig.java
@@ -0,0 +1,49 @@
+package com.avengers.gamera.config;
+
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+
+import java.util.List;
+
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+@Setter
+@ConfigurationProperties(prefix = "cors")
+public class SecurityConfig {
+    private List<String> allowedOrigins;
+    private List<String> allowedMethods;
+    private List<String> allowedHeaders;
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        return http
+                .csrf().disable()
+                .cors().configurationSource(request -> {
+                    var cors = new CorsConfiguration();
+                    cors.setAllowedOrigins(allowedOrigins);
+                    cors.setAllowedMethods(allowedMethods);
+                    cors.setAllowedHeaders(allowedHeaders);
+                    return cors;
+                })
+                .and()
+                .authorizeRequests()
+                .antMatchers("/**").permitAll()
+                .anyRequest().authenticated()
+                .and().build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+}
